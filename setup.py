4107d0a06bf6d0982857b964af167355986688d5
shadr1n <s.h.a.d.r.i.n.mvp@gmail.com>
2024-11-27 12:41:34 +0400

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiogram-callback-manager',
-    version='0.1.0',
+    version='0.1.1',
     author='EvilMathHippy',
     author_email='careviolan@gmail.com',
     description='Асинхронный менеджер callback для aiogram.',
